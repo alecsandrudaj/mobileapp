9d9224930e5f1717da5c74a02cc15c9612392b4e
Razvan Gabriel GOSA <rgosa@bitdefender.com>
2020-11-19 16:03:54 +0200

    server update

---
+++ server/utils/database.py
@@ -0,0 +1,16 @@
+### thil file will handle database conections 
+
+def key_exists(key):
+    return False
+
+def update_database(data):
+    return True
+
+def get_key_for_imei(imei):
+    return 'key'
+
+def register_file(imei, file, filepat):
+    return True
+
+def register_contacts(imei, contacts):
+    return True
